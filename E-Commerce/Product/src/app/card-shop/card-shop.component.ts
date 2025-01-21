84410127219da335dbc54469100dfdc25c25f26d
Rahma Mostafa <rahmamostafa282@gmail.com>
2025-01-21 22:15:24 +0200

    E-Commerce

--- E-Commerce/Product/src/app/card-shop/card-shop.component.ts
+++ E-Commerce/Product/src/app/card-shop/card-shop.component.ts
@@ -1,8 +1,9 @@
-import { CounterService } from './../service/counter-service.service';
 import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CounterService } from '../service/counter-service.service';
 import { HttpService } from '../service/http.service';
 import { CardServiceService } from '../service/card-service.service';
+
 @Component({
   selector: 'app-card-shop',
   imports: [CommonModule],
@@ -10,74 +11,83 @@ import { CardServiceService } from '../service/card-service.service';
   styleUrls: ['./card-shop.component.css'],
 })
 export class CardShopComponent {
-  products: Array<any> = [];
   @Input() id: number = 0;
-
-  counter: number = 0;
+  products: Array<any> = [];
+  cardProductIds: Array<string> = [];
+  filteredProducts: Array<any> = [];
+  productCounters: { [key: number]: number } = {};
+  totalCounter: number = 0;
 
   counterService = inject(CounterService);
   productList = inject(HttpService);
   cardService = inject(CardServiceService);
-  product: any = [];
-  cardProductIds!: Array<string>;
 
   ngOnInit() {
+    // get card product ids
     this.cardService.getCardId().subscribe((ids: string[]) => {
-      this.cardProductIds = ids; // update the local array when the card changes
+      this.cardProductIds = ids;
+      this.filterProducts();
     });
 
-    console.log(this.cardProductIds, ' card product ids from service ');
+    // get all products
+    this.productList.getProductList().subscribe((res) => {
+      this.products = res.products;
+      this.filterProducts();
+    });
 
-    //this.productList.getProductList().subscribe((res) => {
-      //this.products = res.products;
-    //});
+    // total counter
+    this.counterService.getCounter().subscribe((res) => (this.totalCounter = res));
+  }
 
-    if (this.cardProductIds.length) {
-      this.product = this.cardProductIds.find((e: any) => e.id == this.id);
-      if (this.product) {
-        this.product.push(this.id);
-      } else {
-        console.error(`Product with ID ${this.id} not found.`);
-      }
+  filterProducts() {
+    if (this.products.length && this.cardProductIds.length) {
+      this.filteredProducts = this.products.filter((product) =>
+        this.cardProductIds.includes(product.id.toString())
+      );
+
+      this.filteredProducts.forEach((product) => {
+        if (!(product.id in this.productCounters)) {
+          this.productCounters[product.id] = 1;
+        }
+      });
+
+      console.log('Filtered Products:', this.filteredProducts);
+      console.log('Product Counters:', this.productCounters);
+    }
+  }
+
+  decreaseCounter(productId: number) {
+    if (this.productCounters[productId] > 1) {
+      this.productCounters[productId] -= 1;
+      this.totalCounter -= 1;
+      this.counterService.setCounter(this.totalCounter);
+    }
+  }
+
+  increaseCounter(productId: number) {
+    this.productCounters[productId] += 1;
+    this.totalCounter += 1;
+    this.counterService.setCounter(this.totalCounter);
+  }
+
+  deleteCard(productId: number) {
+    if (this.productCounters[productId]) {
+      // decrease the total counter by the quantity of the product being deleted
+      this.totalCounter -= this.productCounters[productId];
+      this.counterService.setCounter(this.totalCounter);
     }
 
-    console.log(this.id, 'this is product id');
-    console.log(this.products);
-    console.log(this.product);
-    console.log(this.id);
+    this.filteredProducts = this.filteredProducts.filter(
+      (product) => product.id !== productId
+    );
 
-    this.counterService.getCounter().subscribe((res) => (this.counter = res));
-    console.log(this.counter);
-  }
-  decreaseCounter() {
-    this.counterService.setCounter(this.counter - 1);
-  }
-  increaseCounter() {
-    this.counterService.setCounter(this.counter + 1);
-  }
-  deleteCounter() {
-    this.counterService.setCounter((this.counter = 0));
+    delete this.productCounters[productId];
+
+    this.cardProductIds = this.cardProductIds.filter(
+      (id) => id !== productId.toString()
+    );
+
+    console.log(`Product with ID ${productId} deleted.`);
+    console.log('Updated Total Counter:', this.totalCounter);
   }
 }
-
-// <div class="product-image">
-//       <img [src]="product.thumbnail" alt="Airpods Max">
-//       <div >
-
-//         <!-- @for (item of product.images; track $index) {
-//           <img [src]="item" alt="Airpods Max" width="100" height="100" >
-//           <br>
-//         } -->
-//       </div>
-
-//       <div class="product-price">{{product.price|currency:'EGP'}}</div>
-
-//       <div class="quantity-control">
-//         <button (click)="decreaseCounter()">-</button>
-//         <input type="text" value={{counter}} min="1" disabled style="font-weight: bold; color:black">
-//         <button (click)="increaseCounter()">+</button>
-//         <button (click)="deleteCounter()">x</button>
-//       </div>
-
-//         <button class="btn btn-light" style="margin: 20px;">Check out</button>
-//     </div>
